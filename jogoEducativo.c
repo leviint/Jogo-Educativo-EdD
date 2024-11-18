b052a0c99d31112d1a1d2771fc1b384c7d07171a
VictorKaue <victorkaue909@gmail.com>
2024-11-18 10:58:11 -0300

    Co-authored-by: JoyceMiranda21 <JoyceMiranda21@users.noreply.github.com> Co-authored-by: Arthur Lemos Bendini <leviint@users.noreply.github.com>

--- jogoEducativo.c
+++ jogoEducativo.c
@@ -1,5 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(){
+typedef struct {
+    int dificuldade;
+    int valor1; 
+    int valor2; 
+    int operacao; 
+    int resultado; 
+}Calcular;
+
+
+
+//Gera um números aleatórios para escolher a operação, atribui na struct Calcular e retorna o valor aleatório.
+int operacaoAleatoria(Calcular *calcular){
+    srand(time(NULL));
+    calcular->operacao = rand() % 3; 
+    return calcular->operacao;
+}
+
+//Gera números aleatórios e atribui no valor1 e valor2 da struct Calcular.
+void valorAleatorio(Calcular *calcular, int dificuldade){
+    srand(time(NULL));
+    int max = 1;
+    for (int i = 0; i < dificuldade; i++){
+        max *= 10;
+    }    
+    calcular->valor1 = rand() % max + 1;
+    calcular->valor2 = rand() % max + 1;
+}
+
+void soma(Calcular *calcular){
+    calcular->resultado = calcular->valor1 + calcular->valor2;
+}
+
+void subtracao(Calcular *calcular){
+    calcular -> resultado = calcular->valor1 - calcular->valor2;
+}
+
+void multiplicacao(Calcular *calcular){
+    calcular->resultado = calcular->valor1 * calcular->valor2;
+}
+
+void historico(Calcular *calcular){
+    int operacoes[10];
+    printf("Seu Historico");
     
 }
+
+void jogar(Calcular *calcular){
+    int pontuacao;
+    int dificuldade = 1;
+    
+    for (int i = 0; i < 10; i++){
+        valorAleatorio(calcular, dificuldade);
+        switch (operacaoAleatoria(calcular)){
+        case 0:
+            soma(calcular);
+            break;
+        case 1:
+            subtracao(calcular);
+            break;
+        case 2:
+            multiplicacao(calcular);
+            break;
+        }
+    }
+    
+}
+
+int main(){
+    Calcular calcular[10];    
+    jogar(&calcular);
+}
+
